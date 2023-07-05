4ae5d7e93c18fca94b5ffd3ea598a7cecbc0d41c
Lisandra Melo <mendie73@gmail.com>
2023-07-05 14:13:44 -0300

    Adição de algumas estatísticas

--- training.py
+++ training.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 
 df_grouped = pd.read_csv('data_files/balanced_sample.csv')
 
@@ -19,8 +19,12 @@ model.fit(X_train, y_train)
 # Fazer previsões usando o modelo treinado
 y_pred = model.predict(X_test)
 
-# Avaliar o desempenho do modelo usando o erro quadrático médio (MSE)
+# Métricas de avaliação
 mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 
-# Imprimir o MSE
-print("Mean Squared Error (MSE):", mse)
+# Impressão das estatísticas
+print("Coeficiente de regressão:", model.coef_)
+print("Intercepto:", model.intercept_)
+print("Erro quadrático médio (MSE):", mse)
+print("Coeficiente de determinação (R²):", r2)
